41dd50cdee1688e1ea19a44abc729b46fe475675
KeitaYamadaJapan <43283597+KeitaYamadaJapan@users.noreply.github.com>
2023-01-09 09:00:04 +0900

    Add files via upload

---
+++ DFS E - Count Simple Paths_Text/E - Count Simple Paths_Text.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  E - Count Simple Paths_Text
+//
+//  Created by 山田啓太 on 2023/01/08.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int calc(int N, vector<vector<int>> g){
+    int ans = 0, limit = 1000000;
+    vector<int> vis(N);
+    auto dfs = [&](auto rc,int c) -> void {
+        if(ans ==  limit) return;
+        ans++;
+        vis[c] = true;
+        for(auto& d : g[c]){
+            if(vis[d]) continue;
+            rc(rc, d);
+        }
+        vis[c] = false;
+    };
+    dfs(dfs, 0);
+    return ans;
+}
+
+int main(){
+    cout << endl;
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> g(N);
+    for(int i = 0; i < M; i++){
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    cout << calc(N, g) << endl;
+    return 0;
+}
+
+
+/**
+ 
+ https://atcoder.jp/contests/abc284/tasks/abc284_e
+ 
+ https://atcoder.jp/contests/abc284/editorial/5494
+ 
+ */
+
+
+
+
+
